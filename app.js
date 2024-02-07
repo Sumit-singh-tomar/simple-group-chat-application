c5c14bcdb675e0d7d3e51a40c3c8a77db01d22be
sumit singh tomar <sumitsinght10@gmail.com>
2024-02-07 23:32:39 +0530

    save in localhost

--- app.js
+++ app.js
@@ -9,23 +9,24 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/login', (req, res, next) => {
     res.send(`
-            <form id="loginForm" action="/" method="POST">
+            <form id="loginForm" action="/" onsubmit="handleSubmit(event)" method="POST">
                 <input type="text" name="username" id="username" placeholder="Enter User Name" />
                 <button type="submit">Login</button>
              </form>
 
              <script>
-                const loginForm = document.getElementById('loginForm')
-                loginForm.addEventListener("submit", ()=>{
+             function handleSubmit(event){
                     event.preventDefault()
                     var username = document.getElementById('username').value
                     localStorage.setItem("username", username)
-                })
+                    event.target.submit()
+                }
              </script> 
              `)
 })
+
 app.use('/message', (req, res, next) => {
-    fs.appendFile("username.txt", (e) => {
+    fs.appendFile("username.txt", `${req.body.username}: ${req.body.message}`, (e) => {
         res.redirect('/')
     })
 })
@@ -34,10 +35,43 @@ app.use('/', (req, res, next) => {
     fs.readFile("username.txt", (err, data) => {
         if (err) {
             console.log(err)
-            res.send('<form action="/message" method="POST"><input type="text" name="message"  placeholder="Enter Message" /> <button type="submit">Submit</button> </form> </body>')
+            res.send(`
+                <form action="/message" method="POST" onsubmit="handleSubmit(event)">
+                    <input type="text" name="message"  placeholder="Enter Message" />
+                    <input type="hidden" id="username" name="username" />
+                    <button type="submit">Submit</button> 
+                </form>
+    
+                <script>
+                    function handleSubmit(event){
+                        event.preventDefault()
+                        var username = localStorage.getItem("username")
+                        document.getElementById('username').value = username
+                        event.target.submit()
+                    }
+                </script>    
+            `)
         }
         else {
-            res.send(`<div>${data} <form action="/message" method="POST"><input type="text" name="message"  placeholder="Enter Message" /> <button type="submit">Submit</button> </form> </body> </div>`)
+            res.send(`
+            <div>
+                ${data} 
+                <form action="/message" method="POST" onsubmit="handleSubmit(event)">
+                    <input type="text" name="message"  placeholder="Enter Message" />
+                    <input type="hidden" id="username" name="username" />
+                    <button type="submit">Submit</button> 
+                </form>
+    
+                <script>
+                    function handleSubmit(event){
+                        event.preventDefault()
+                        alert('dlkjf')
+                        var username = localStorage.getItem("username")
+                        document.getElementById('username').value = username
+                        event.target.submit()
+                    }
+                </script>    
+            </div>`)
         }
     })
 })
